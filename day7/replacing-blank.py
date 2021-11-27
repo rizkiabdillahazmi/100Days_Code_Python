1fb65976fd984de4dccf01d5580e6243711eef83
rizkiabdillahazmi <rizkiabdillahazmi10@gmail.com>
2021-11-27 18:09:45 +0700

    update

---
+++ day7/replacing-blank.py
@@ -0,0 +1,23 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+# Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for letter in chosen_word:
+    display += "_"
+print(display)
+
+end_game = False
+while not end_game:
+    guess = input("Guess a letter: ").lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+    if "_" not in display:
+        end_game = True
+        print("You Win!")
