ceaeed2441f1f9196a2df4d6b7896e0e151f7c52
rizkiabdillahazmi <rizkiabdillahazmi10@gmail.com>
2021-11-27 19:11:41 +0700

    update day8

---
+++ function_input.py
@@ -0,0 +1,7 @@
+def greet(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location} ?")
+
+
+greet("Rizki Abdillah Azmi", "Banda Aceh")
+greet(location="Banda Aceh", name="Rizki")
