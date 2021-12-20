25a5e0ceb1f4f39ef59efc703ed0f335b8af74c1
rizkiabdillahazmi <rizkiabdillahazmi10@gmail.com>
2021-12-20 11:52:12 +0700

    update coffee machine OOP

--- 016-day16/prettytable_lesson.py
+++ 016-day16/prettytable_lesson.py
@@ -5,4 +5,7 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
+
+# Object Attribute
+table.align = "l"
 print(table)
